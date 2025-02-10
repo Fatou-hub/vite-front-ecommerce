f54394fcf884121a340ca2a578814cd4f0e8db58
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2025-02-10 20:32:13 +0100

    chore(eslint): update of dependencies

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,7 +2,7 @@ import type { UserConfig } from '@commitlint/types';
 import { RuleConfigSeverity } from '@commitlint/types';
 
 const Configuration: UserConfig =  {
-    extends : ['@commitlint/config-conventionnal'],
+    extends : ['@commitlint/config-conventional'],
     rules: {
         'scope-empty': [RuleConfigSeverity.Error, 'never'],
     },
