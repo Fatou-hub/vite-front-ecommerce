2587afd4f8a5c309cd307b13cc27d21d2455e381
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2025-02-11 14:15:54 +0100

    chore(conventionalcommit): fixing dependencies of conventional commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -6,7 +5,6 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
-  const bla = "bla";
 
   return (
     <>
