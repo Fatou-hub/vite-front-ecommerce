dc7468b21cf219c2d6252ca73f5ef09288613ea4
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2025-02-28 09:51:52 +0100

    feat(search-product): searchbar storage

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import ProductSearch from "./features/product/search-products/ui/ProductSearch";
 
 
 function App() {
+  const onSubmit = (search : string) => {
+    console.log(search);
+  }
+
   return (
-    <ProductSearch/>
+    <ProductSearch onSubmit={onSubmit}/>
   );
 };
 
