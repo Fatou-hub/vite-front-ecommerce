762cdc5be5c6244164e7a6bcfe71e1664a4ef69d
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2025-02-28 14:06:58 +0100

    feat(list-products): product item

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import Header from "./common/layout/Header";
+import ProductItem from "./features/product/list-products/ui/ProductItem";
 
 function App() {
-  const onSubmit = (search : string) => {
+  const onSubmit = (search: string) => {
     console.log(search);
   };
 
   const cartCount = 2;
 
+  const product = {
+    name: 'Diaper new born Baby',
+    picture: '',
+    price: 1000,
+  }
+
   return (
-    <Header onSubmit={onSubmit} cartCount={cartCount}/>
+    <>
+      <Header onSubmit={onSubmit} cartCount={cartCount} />
+      <ProductItem product={product} />
+    </>
+
   );
 };
 
