8f61fd7c41e68b86a9f46f5e802fe0b908b2aae1
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2025-03-05 10:29:16 +0100

    feat(list-cart-products): cart page with list of product selected

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import type { ProducItemData } from "./features/product/list-products/types";
 import Header from "./common/layout/Header";
 import Product from "./features/product/display-product/ui/Product/Product";
 import type { ProductData } from "./features/product/display-product/types";
+import CartProductTem from "./features/cart/list-cart-products/ui/CartProductItem";
+import { cartProductItemData } from "./features/cart/list-cart-products/types";
 
 
 
@@ -18,39 +20,47 @@ function App() {
       name: 'Premium Diapers',
       picture: '',
       price: 5,
-      },
-      {
+    },
+    {
       id: '2',
       name: 'Gentle Wipes',
       picture: '',
       price: 50,
-      },
-      {
+    },
+    {
       id: '3',
       name: 'Diapers Pants',
       picture: '',
       price: 34,
-      },
+    },
   ];
 
 
   const product: ProductData = {
-    name: "MY Baby Diapers",
-    picture:"",
+    name: "MYBABY Diapers",
+    picture: "",
     price: 4,
-    description:"These diapers are made for babies from 0 to 6 months. It offers soft and toxic free components for the safety of your baby butt. Our MYBABY diapers respects the sensitiveness of a baby skin especially the down area after doing the duty. Yes there are NO : WITHOUT LOTION, PERFUME, PETROLATUM; TCF cellulose pulp from responsibly managed FSC® certified forests and other controlled sources; Backsheet sails maintenance and acquisition-receiver",
+    description: "These diapers are made for babies from 0 to 6 months. It offers soft and toxic free components for the safety of your baby butt. Our MYBABY diapers respects the sensitiveness of a baby skin especially the down area after doing the duty. Yes there are NO : WITHOUT LOTION, PERFUME, PETROLATUM; TCF cellulose pulp from responsibly managed FSC® certified forests and other controlled sources; Backsheet sails maintenance and acquisition-receiver",
   }
 
-const addToCart = (): void =>{
-  console.log('Add to cart');
-}
+  const addToCart = (): void => {
+    console.log('Add to cart');
+  }
+
+  const cartProduct : Omit<cartProductItemData, 'id'> = {
+    name: 'MYBABY Diapers ',
+    picture: '',
+    price: 4,
+  }
+
+  const removeFromCart = () : void => {
+    console.log('Remove from cart');
+  }
 
   return (
     <>
       <Header onSubmit={onSubmit} cartCount={cartCount} />
-      <div className="mt-5">
-      <Product product={product} addToCart={addToCart} />
-      </div>
+      <CartProductTem cartProduct={cartProduct} removeFromCart={removeFromCart} />
     </>
 
   );
