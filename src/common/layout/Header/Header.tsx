71178630b34d9da81c99a5c564e761333bad5a09
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2025-03-07 14:46:34 +0100

    feat(routing): setting up my first routes

--- src/common/layout/Header/Header.tsx
+++ src/common/layout/Header/Header.tsx
@@ -1,24 +1,48 @@
 import type { ReactElement } from "react";
-
+import { useMediaQuery } from "@mui/material";
 import ProductSearch from "../../../features/product/search-products/ui/ProductSearch";
 import CartCount from "../../../features/cart/display-cart-count/ui/CartCount";
+import { Link } from "react-router-dom";
 
 
 type Props = {
     onSubmit: (search: string) => void;
-    cartCount:number;
+    cartCount: number;
 }
 
-const Header = ({onSubmit, cartCount}: Props): ReactElement => {
-    return(
-        <header className="p-4 flex flex-col lg:flex-row-reverse lg:items-center justify-center bg-primary ">
-            <div className="flex justify-end">
-            <CartCount cartCount={cartCount}/>
-            </div>
+const Header = ({ onSubmit, cartCount }: Props): ReactElement => {
+    return (
+        <header className="p-4 bg-primary">
+            {useMediaQuery('(max-width: 1023px)') && (
+                <>
+                    <div className="mb-2 flex justify-between">
+                        <Link to="/" className="text-white font-bold">
+                            Site
+                        </Link>
 
-            <div className="flex justify-center w-full lg:w-10/12">
-            <ProductSearch onSubmit={onSubmit} />
-            </div>
+                        <Link to="/cart">
+                        <CartCount cartCount={cartCount} />
+                        </Link>
+                    </div>
+                    <ProductSearch onSubmit={onSubmit} />
+                </>
+            )}
+
+            {useMediaQuery('(min-width: 1024px)') && (
+                <>
+                    <div className="flex items-center justify-center gap-4 lg:w-4/5">
+                        <Link to="/" className="text-white font-bold">
+                            Site
+                        </Link>
+
+                        <Link to="/cart">
+                        <CartCount cartCount={cartCount} />
+                        </Link>
+                        
+                    </div>
+                    <ProductSearch onSubmit={onSubmit} />
+                </>
+            )}
         </header>
     );
 
--- src/features/product/list-products/ui/ProductList/ProductList.tsx
+++ src/features/product/list-products/ui/ProductList/ProductList.tsx
@@ -1,18 +1,23 @@
 import type { ReactElement } from "react";
 import { ProducItemData } from "../../types";
 import ProductItem from "../ProductItem";
+import { Link } from "react-router-dom";
 
 type Props = {
     products: ProducItemData[];
 }
 
-const ProductList = ({ products }: Props): ReactElement =>{
-    return(
+const ProductList = ({ products }: Props): ReactElement => {
+    return (
         <div className="flex flex-col items-center sm:flex-row sm:flex-wrap sm:justify-center">
-            {products.map((product)=> {
+            {products.map((product) => {
                 const { id, ...productWithoutId } = product;
-            
-               return <ProductItem key={id} product={productWithoutId}/>;
+
+                return (
+                    <Link key={id} to={`/product/${id}`}>
+                        <ProductItem product={productWithoutId} />;
+                    </Link>
+                );
             })}
         </div>
     )
