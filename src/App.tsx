233ecf576f49818939bb6698dd349aebe5d274da
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2025-03-18 15:31:59 +0100

    refactor(cartproduct): cart updating with useState

--- src/App.tsx
+++ src/App.tsx
@@ -13,24 +13,28 @@ import ProductList from "./features/product/list-products/ui/ProductList";
 function App(): ReactElement {
   const productToDisplay: Record<string, ProductData> = {
     '1':{
+      id:"1",
       name: "MYBABY Premium Diapers",
       picture: "",
       price: 5,
       description:"These diapers are made for babies from 12 to 28 months. It offers soft and toxic free components for the safety of your baby butt. Our MYBABY diapers respects the sensitiveness of a baby skin especially the down area after doing the duty. Yes there are NO : WITHOUT LOTION, PERFUME, PETROLATUM; TCF cellulose pulp from responsibly managed FSC® certified forests and other controlled sources; Backsheet sails maintenance and acquisition-receiver",
     },
     '2':{
+      id:"2",
       name: 'MYBABY Gentle wipes',
       picture: '',
       price: 2,
       description:"It is made for newborns, babies, and toddlers, Fragrance Free, Hypoallergenic, Safe for sensitive skin ",
     },
     '3':{
+      id:"3",
       name: 'MYBABY Pants',
       picture: '',
       price: 10,
       description:"Up to 100% leakproof protection with Dual Leak-Guard Barriers 360 stretchy waistband, Soft and comfy, underwear like fit, Dermatologically tested and hypoallergenic",
     },
     '4':{
+      id:"4",
       name: 'MYBABY Diapers',
       picture: '',
       price: 10,
@@ -144,7 +148,7 @@ const changeURL = (productId: string): void =>{
         
         <Route path="/" element={<ProductList products={products}/>}/>
 
-        <Route path="/product/:id" element={<Product product={product} addToCart={addToCart}/>}/>
+        <Route path="/product/:id" element={<Product product={product} addToCart={()=> addToCart(product.id)}/>}/>
 
         <Route path="/cart" element={<CartProductList cartProducts={cartProducts} removeFromCart={removeFromCart}/>}/>
 
