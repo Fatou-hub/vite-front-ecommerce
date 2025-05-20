4cd3345a1330237b53e6f69923b38822009abc4d
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2025-05-20 10:46:13 +0200

    feat(api): import of API calls to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,126 @@
-import { useState, ReactElement, useEffect } from "react";
-import type { ProductItemData } from "./features/product/list-products/types";
-import Header from "./common/layout/Header";
-import Product from "./features/product/display-product/ui/Product/Product";
-import type { ProductData } from "./features/product/display-product/types";
-import { CartProductItemData } from "./features/cart/list-cart-products/types";
-import CartProductList from "./features/cart/list-cart-products/ui/CartProductList";
-import { Routes, Route, useMatch } from "react-router-dom";
-import ProductList from "./features/product/list-products/ui/ProductList";
-import { allProducts } from "./features/product/list-products/data/allProducts";
-import { productToAddToCart } from "./features/cart/add-cart-product/data/productToAddToCart";
-import { productToDisplay } from "./features/product/display-product/data/productToDisplay";
+
+
+import type { ReactElement } from 'react';
+import { useEffect, useState } from 'react';
+import { Routes, Route, useMatch } from 'react-router-dom';
+import type { ProductItemData } from './features/product/list-products/types';
+import type { ProductData } from './features/product/display-product/types';
+import type { CartProductItemData } from './features/cart/list-cart-products/types';
+import { allProducts } from './features/product/list-products/data/allProducts';
+import { productToDisplay } from './features/product/display-product/data/productToDisplay';
+import { getProducts } from './features/product/list-products/api/getProducts';
+import { getProduct } from './features/product/display-product/api/getProduct';
+import { searchProducts } from './features/product/search-products/api/searchProducts';
+  import {addProductToCart,getCartCount,getCartProducts, removeProductFromCart} from './features/cart/api/cart';
+import Header from './common/layout/Header';
+import ProductList from './features/product/list-products/ui/ProductList';
+import Product from './features/product/display-product/ui/Product/Product';
+import CartProductList from './features/cart/list-cart-products/ui/CartProductList';
+
 
 
 function App(): ReactElement {
 
+  const matchHomePage = useMatch('/');
   const matchProductPage = useMatch('/product/:id');
-  const [products, setProducts] = useState<ProductItemData[]>(allProducts);
-  const [product, setProduct] = useState<ProductData>(productToDisplay['1']);
-  const [cartProducts, setCartProducts] = useState<CartProductItemData[]>([]);
+  const matchCartPage = useMatch('/cart');
+
+  const [products, setProducts] =
+    useState<ProductItemData[]>(allProducts);
+
+  const [product, setProduct] =
+    useState<ProductData>(productToDisplay['1']);
+
+  const [cartProducts, setCartProducts] =
+    useState<CartProductItemData[]>([]);
+
   const [cartCount, setCartCount] = useState<number>(0);
 
-  const onSubmit = (search: string): void => {
-    const filteredProducts = allProducts.filter((product)=>
-
-    product.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
-  );
-  setProducts(filteredProducts);
+  const onSubmit = async (search: string): Promise<void> => {
+    const filteredProducts = await searchProducts(search);
+    setProducts(filteredProducts);
   };
 
-  const addToCart = (productId: string): void => {
-    const isProductInCart = cartProducts.some(
-      (product)=> product.id === productId
-    );
-
-    if(!isProductInCart){
-      const productToAdd = productToAddToCart[productId];
-
-      setCartProducts([...cartProducts, productToAdd]);
-      setCartCount(cartCount + 1);
-    }
+  const addToCart = async (productId: string): Promise<void> => {
+    const updatedCart = await addProductToCart(productId);
+    setCartProducts(updatedCart.cartProducts);
+    setCartCount(updatedCart.cartCount);
   };
 
-  const removeFromCart = (productId: CartProductItemData['id']) : void => {
-    const cartProductWithoutTheRemovedOne = cartProducts.filter(
-      (cartProduct) => cartProduct.id !== productId,
-    );
+  const removeFromCart = async (
+    productId: CartProductItemData['id'],
+  ): Promise<void> => {
+    const updatedCart = await removeProductFromCart(productId);
 
-     setCartProducts(cartProductWithoutTheRemovedOne);
-     setCartCount(cartCount - 1);
-  }
+    setCartProducts(updatedCart.cartProducts);
+    setCartCount(updatedCart.cartCount);
+  };
 
-const changeURL = (productId: string): void => {
-  setProduct(productToDisplay[productId]);
-};
+  const fetchProduct = async (productId: string): Promise<void> => {
+    const product = await getProduct(productId);
+    setProduct(product);
+  };
 
-  useEffect(()=>{
-    const ProductIdInURL = matchProductPage?.params.id;
+  const fetchProducts = async (): Promise<void> => {
+    const initialProducts = await getProducts();
+    setProducts(initialProducts);
+  };
 
-    if(ProductIdInURL) {
-      changeURL(ProductIdInURL);
+  const fetchCartCount = async (): Promise<void> => {
+    const initialCartCount = await getCartCount();
+    setCartCount(initialCartCount);
+  };
+
+  const fetchCartProducts = async (): Promise<void> => {
+    const initialCartProducts = await getCartProducts();
+    setCartProducts(initialCartProducts);
+  };
+
+  useEffect(() => {
+    fetchCartCount();
+  }, []);
+
+  useEffect(() => {
+    if (matchHomePage) fetchProducts();
+  }, [matchHomePage]);
+
+  useEffect(() => {
+    if (matchCartPage) fetchCartProducts();
+  }, [matchCartPage]);
+
+  useEffect(() => {
+    const productIdInURL = matchProductPage?.params.id;
+    if (productIdInURL) {
+      fetchProduct(productIdInURL);
     }
   }, [matchProductPage]);
-
-
+  
   return (
     <>
       <Header onSubmit={onSubmit} cartCount={cartCount} />
       <Routes>
-        
-        <Route path="/" element={<ProductList products={products}/>}/>
-
-        <Route 
-        path="/product/:id" 
-        element={
-        <Product 
-          product={product} 
-          addToCart={()=> addToCart(product.id)}
-          />
-          }/>
-
-        <Route 
-          path="/cart" 
+        <Route path="/" element={<ProductList products={products} />}
+        />
+        <Route
+          path="/product/:id"
           element={
-            <CartProductList 
-              cartProducts={cartProducts} 
-              removeFromCart={removeFromCart}
-              />
-              }
+            <Product
+              product={product}
+              addToCart={() => addToCart(product.id)}
             />
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <CartProductList
+              cartProducts={cartProducts}
+              removeFromCart={removeFromCart}
+            />
+          }
+        />
       </Routes>
     </>
   );
-};
-
+}
 export default App;
-
